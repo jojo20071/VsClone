adde3ed8fdc9a24f40de19abaaf6f440774f6f7d
Johannes Häderle <83701560+jojo20071@users.noreply.github.com>
2024-08-14 14:21:38 +0200

    2

---
+++ scripts.js
@@ -0,0 +1,6 @@
+onst fileInput = document.getElementById('fileInput');
+const fileList = document.getElementById('fileList');
+const tabList = document.getElementById('tabList');
+const editor = document.getElementById('editor');
+const terminalInput = document.getElementById('terminal-input');
+const terminalOutput = document.getElementById('terminal-output');
